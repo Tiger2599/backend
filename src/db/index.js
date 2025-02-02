41e38a60a81789f1010881b144bfd1d84d2fe27f
tiger2599 <parasbanbhiya@gmail.com>
2025-02-02 09:33:18 +0530

    app configer with DB

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB Connected: ${ConnectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MONGODB Connection error: ", error.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config();
+connectDB()
+.then(() => {
+	app.listen(process.env.PORT || 8000, () => {
+		console.log(`Server is running on port http://localhost:${process.env.PORT || 8000}`);
+	});
+})
+.catch(err=>console.error(err.message));
